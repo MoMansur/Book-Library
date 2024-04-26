fe20f63da01792b5a17c088718e194ae25338f26
Mansur Shehu <mansurshehumsm@gmail.com>
2024-04-26 14:49:09 +0300

    test

--- main.js
+++ main.js
@@ -22,96 +22,74 @@ const addNewBookBtn = document.getElementById('AddNewBook')
 
 
 
-const myLibrary = [];
-class Book{
-    constructor(title, author, pages, read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read
-    }
-    readStatus(){
-        let readStatusStr = this.read ? "read" : "not read yet";
-         
-    }
-}
-
-function addBookToLibrary() {
-}
-
-
-const titleSpace = document.getElementById('titleSpace')
-const authorSpace = document.getElementById('authorSpace')
-const numOfPagesSpace = document.getElementById('numOfPagesSpace')
-
-
-function test(){
-    console.log('I ahve been clicked')
-}
-
-
-function newBook(title,author,pages,read){
-
-    const testBook = new Book(title,author,pages,read)
-    myLibrary.push(testBook)
-
-for(let i = 0; i<myLibrary.length; i++){
-
-    bookSpace.innerHTML += `<div class="bookInfoBox">
-    <p class="fs-3" id="titleSpace">${myLibrary[i].title} </p>
-    <p id="authorSpace">Author: ${myLibrary[i].author} </p>
-    <p id="numOfPagesSpace">Number of Pages:  ${myLibrary[i].pages} </p>
-    <p>Status:Not read </p>
-    <button id="editInfoBtn" class="btn btn-secondary" type="submit">Edit Info</button>
-                        <button id="removeBook" class="btn btn-danger">Remove</button>
-                        </div>`;
-
-
-
-    console.log(testBook) 
-}
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    bookSpace.innerHTML = ""
-    newBook(titleForm.value,authorForm.value,numOfPagesForm.value,'not yet')
-
- 
-   
-})
-
-
-
-
-
-
-
-myLibrary.push({
-    title: '48 Laws of Power',
-    author: 'Robert Greene',
-    pages: 412,
-    read:true
-},{
-    title: 'Pride and Prejudice',
-    author: 'Jane Austen',
-    pages: 620,
-    read:true
-},
-{
-    title: 'The Great Gatsby',
-    author: ' Scott Fitzgerald',
-    pages: 935,
-    read:true
- })
-
 
 
 
 
  function creation(){
-    const newDiv = document.createElement("div")
-    document.body.appendChild(newDiv)
+
+    for(let i =0; i<2; i++){
+
+    
+    const newModel = document.createElement("div")
+    newModel.className = "bookInfoBox"
+
+    const pForTitle =document.createElement("p")
+    pForTitle.className = "fs-3";
+    pForTitle.textContent = 'Title'
+
+    const pForAuthor = document.createElement("p")
+    pForAuthor.textContent = "Author: "
+
+    const pForPages = document.createElement("p")
+    pForPages.textContent = "Num of Pages: "
+
+    const pForStatus = document.createElement("p")
+    pForStatus.textContent = "Have you read?: "
+
+
+    const statusYes =document.createElement("input")
+    statusYes.className = "form-check form-switch";
+    statusYes.type = "checkbox"
+    statusYes.role = "switch"
+ 
+
+    const btnForEdit = document.createElement("button")
+    btnForEdit.className = "btn btn-secondary m-1"
+    btnForEdit.type = "submit";
+    btnForEdit.textContent = "Edit Info"
+
+    const btnForRemove =document.createElement("button")
+    btnForRemove.className = "btn btn-danger"
+    btnForRemove.type = "button";
+    btnForRemove.textContent = "Remove Book"
+   
+
+
+bookSpace.appendChild(newModel)
+
+newModel.appendChild(pForTitle)
+newModel.appendChild(pForAuthor)
+newModel.appendChild(pForPages)
+newModel.appendChild(pForStatus)
+
+newModel.appendChild(statusYes)
+
+newModel.appendChild(btnForEdit)
+newModel.appendChild(btnForRemove)
+
+
+
+    }
+  
  }
  creation()
+
+//  <div class="bookInfoBox">
+//  <p class="fs-3" id="title"> </p>
+//  <p>Author:  </p>
+//  <p>Number of Pages:   </p>
+//  <p>Status:Not read </p>
+//  <button id="editInfoBtn" class="btn btn-secondary" type="submit">Edit Info</button>
+//  <button id="removeBook" class="btn btn-danger">Remove</button>
+// </div>
